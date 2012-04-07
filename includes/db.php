98dda487891435bdbf5a3e4b759c45fd8685fefc
pixelles <patriciadupelle@msn.com>
2012-04-06 20:08:05 -0400

    added stripslashes

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$dsn = getenv('DB_DSN');
+$dsn = stripslashes(getenv('DB_DSN'));
 
 $db = new PDO($dsn, $user, $pass);
 
