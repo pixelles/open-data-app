c14a098f331c20327c5ae3d75d306abb24482e5d
pixelles <patriciadupelle@msn.com>
2012-04-13 08:59:00 -0400

    Reinserted redirect to sign in page

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,10 @@
 require_once '../includes/db.php';
 require_once '../includes/users.php';
 
+if (!user_is_signed_in()) {
+	header('Location: sign-in.php');
+	exit;
+}
 
 $results = $db->query('
 	SELECT id, name, street_address, longitude, latitude
