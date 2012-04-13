63850c55d0d1b583afec74b6ecc07cfc925502ee
pixelles <patriciadupelle@msn.com>
2012-04-13 08:45:37 -0400

    Created a user for my professor to log into the administration panel.

--- admin/sign-in.php
+++ admin/sign-in.php
@@ -55,11 +55,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <form method="post" action="sign-in.php">
 	<div>
-    	<label for="email">E-mail Address</label>
+    	<label for="email">E-mail Address<?php if (isset($errors['email'])) : ?> <strong>is required</strong><?php endif; ?><?php if (isset($errors['user-non-existent'])) : ?> <strong>This user does not exist</strong><?php endif; ?></label>
         <input type="email" id="email" name="email" required>
 	</div>
 	<div>
-		<label for="password">Password <?php if (isset($errors['password-no-match'])) : ?> <strong>Passwords don't match</strong><?php endif; ?></label>
+		<label for="password">Password<?php if (isset($errors['password'])) : ?> <strong>is required</strong><?php endif; ?><?php if (isset($errors['password-no-match'])) : ?> <strong>Passwords don't match</strong><?php endif; ?></label>
 		<input type="password" id="password" name="password" required>
 	</div>
 	<button type="submit">Sign In</button>
