df0533ccce0439fb745a9ca3c987e0fbcf8f842e
pixelles <patriciadupelle@msn.com>
2012-04-15 10:04:11 -0400

    Removed to be replaced by header and footer for better communication.

--- includes/admin-bottom.php
+++
@@ -1,7 +0,0 @@
-</body>
-</html>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCkXYdCxOIr9-DwpF18ejWqV8C01jbmgxA&sensor=false"></script>
-<script src="js/splash-pad-locator.js"></script>
-<script src="js/latlng.min.js"></script>
