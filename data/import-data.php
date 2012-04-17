f72e793e4693edb825aacbada6baa8b76399e0b4
pixelles <patriciadupelle@msn.com>
2012-04-16 22:24:20 -0400

    Added street_address variable and code to load the new KML file.

--- data/import-data.php
+++ data/import-data.php
@@ -5,13 +5,30 @@ require_once '../includes/db.php';
 $places_xml = simplexml_load_file('splash_pads.kml');
 
 $sql = $db->prepare('
-	INSERT INTO locations (name, longitude, latitude)
-	VALUES (:name, :longitude, :latitude)
+	INSERT INTO locations (name, longitude, latitude, street_address)
+	VALUES (:name, :longitude, :latitude, :street_address)
 ');
 
-foreach ($places_xml->Document->Folder[0]->Placemark as $place) {
+foreach ($places_xml->Document->Placemark as $place) {
+	
+	$name = '';
+	$street_address = '';
+	
+	foreach ($place->ExtendedData->Data as $civic) {
+		if ($civic->attributes()->name == 'Name') {
+			$name = ucwords(strtolower($civic->value));
+		}
+		
+		if ($civic->attributes()->name == 'Address') {
+		$street_address = ucwords(strtolower($civic->value));
+		}
+
+	}
+	
 	$coords = explode(',', trim($place->Point->coordinates));
-	$sql->bindValue(':name', $place->name, PDO::PARAM_STR);
+	
+	$sql->bindValue(':name', $name, PDO::PARAM_STR);
+	$sql->bindValue(':street_address', $street_address, PDO::PARAM_STR);
 	$sql->bindValue(':longitude', $coords[0], PDO::PARAM_STR);
 	$sql->bindValue(':latitude', $coords[1], PDO::PARAM_STR);
 	$sql->execute();
