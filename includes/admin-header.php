3a0aa9ea6768c2eb4750c55a0255a3d7f3e7b694
pixelles <patriciadupelle@msn.com>
2012-04-17 23:08:53 -0400

    Inserted stylesheet reference

--- includes/admin-header.php
+++ includes/admin-header.php
@@ -1,18 +1,21 @@
 <!DOCTYPE HTML>
-<html>
+<html lang=en-ca>
 <head>
-	<meta charset="utf-8">
+	<meta charset=utf-8>
 	<title><?php if (isset($title)) { echo $title . ' · '; } ?>Ottawa's Splendid Splash Pad Locator</title>
+	<link href="../css/admin.css" rel="stylesheet">
+	<script src="js/modernizr.dev.js"></script>
 </head>
 <body>
+
 	<header>	
 		<h1>Ottawa's Splendid Splash Pad Locator</h1>
 		<nav>
 			<h2>Navigation</h2>
 			<ul>
-				<li><a href="../index.php">Home</a></li>
-				<li><a href="index.php">Administration</a></li>
-				<li><a href="sign-out.php">Sign Out</a></li>
+				<li><a href="../index.php">Home</a></li> &middot; 
+				<li><a href="index.php">Administration</a></li> &middot; 
+				<li><a href="sign-out.php">Sign Out</a></li> &middot; 
 				<li><a href="http://imm.edumedia.ca/dupe0012/open-data-app">Project Brief</a>
 			</ul>
 		</nav>
