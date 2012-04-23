9fddc62a5628ed1497f1239018f06705122771d2
pixelles <patriciadupelle@msn.com>
2012-04-23 16:59:47 -0400

    Added a description.

--- admin/sign-in.php
+++ admin/sign-in.php
@@ -1,5 +1,16 @@
 <?php 
 
+/**
+* Description: sign-in page featuring a form for the user to sign-in before viewing the administration page. If user is already signed in, they will be redirected to the administration page.
+*
+* @package com.trishajessica.splashpadlocator
+* @copyright 2012 Trisha Jessica
+* @author Trisha Jessica <hello@trishajessica.ca>
+* @link <http://www.pixelles.github.com/open-data-app>
+* @license New BSD License <https://github.com/pixelles/open-data-app/blob/master/LICENSE.txt>
+* @version <https://github.com/pixelles/open-data-app/blob/master/VERSION.txt>
+*/
+
 require_once '../includes/db.php';
 require_once '../includes/users.php';
 
