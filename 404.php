fabfb454071f2dfc0c9291e86eb7ccaba230110a
pixelles <patriciadupelle@msn.com>
2012-04-23 17:03:42 -0400

    Added a 404 Error Page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'includes/header.php';
+
+?>
+	<section>
+		<h2>Page not found</h2>
+		<p>Oops! Looks like the page you are looking for does not exist.</p>
+		<p><a href="index.php" class="home-link">Go back to home page</a></p>
+	</section>
+
+<?php
+
+require_once 'includes/footer.php';
+
+?>
+
