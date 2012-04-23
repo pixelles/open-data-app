4c92f4a974fbbfa4b90549d2bafcd8a58e82183e
pixelles <patriciadupelle@msn.com>
2012-04-23 17:03:11 -0400

    Added if statement so only signed in users can access and a description

--- admin/edit.php
+++ admin/edit.php
@@ -1,6 +1,23 @@
 <?php
 
+/**
+* Description: allows the user to edit an already existing splash pad location.
+*
+* @package com.trishajessica.splashpadlocator
+* @copyright 2012 Trisha Jessica
+* @author Trisha Jessica <hello@trishajessica.ca>
+* @link <http://www.pixelles.github.com/open-data-app>
+* @license New BSD License <https://github.com/pixelles/open-data-app/blob/master/LICENSE.txt>
+* @version <https://github.com/pixelles/open-data-app/blob/master/VERSION.txt>
+*/
+
 require_once '../includes/db.php';
+require_once '../includes/users.php';
+
+if (!user_is_signed_in()) {
+	header('Location: sign-in.php');
+	exit;
+}
 
 $errors = array();
 
