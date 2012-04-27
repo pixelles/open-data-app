274cf5206c4672f163e0ea1dda640761159ae852
pixelles <patriciadupelle@msn.com>
2012-04-27 19:13:48 -0400

    Added additional Disquis code

--- single.php
+++ single.php
@@ -90,13 +90,14 @@ require_once 'includes/header.php';
     <div class="back"> <a href="index.php">Back</a> </div>
 	
 </section>
+<script type="text/javascript" src="http://trishajessica.disqus.com/combination_widget.js?num_items=5&hide_mods=0&color=blue&default_tab=people&excerpt_length=200"></script><a href="http://disqus.com/">Powered by Disqus</a>
 
-<section id="disquis_thread" class="clearfix">
+<div id="disquis_thread" class="clearfix">
 
 <script type="text/javascript">
 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 var disqus_shortname = 'trishajessica'; // required: replace example with your forum shortname
-var disqus_developer = 1; // developer mode is on
+var disqus_developer = 0; // developer mode is on
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
 var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
@@ -107,6 +108,10 @@ dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
 
-</section>
+</div>
+
+<?php if (sfConfig::get('sf_environment') == 'dev'): ?>
+  var disqus_developer = 1;
+<?php endif ?>
 
 <?php require_once 'includes/footer.php'; ?>
