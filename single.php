2dcdb122ba11fe2f738ae414558fb2c76d4229c8
pixelles <patriciadupelle@msn.com>
2012-04-27 19:01:43 -0400

    Added Disquis comments

--- single.php
+++ single.php
@@ -91,4 +91,22 @@ require_once 'includes/header.php';
 	
 </section>
 
+<section id="disquis_thread" class="clearfix">
+
+<script type="text/javascript">
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'trishajessica'; // required: replace example with your forum shortname
+var disqus_developer = 1; // developer mode is on
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+
+</section>
+
 <?php require_once 'includes/footer.php'; ?>
