f6abc2449bfbdf6e668899c601947f72af3687ce
pixelles <patriciadupelle@msn.com>
2012-04-27 16:25:10 -0400

    Added proper file locations for images

--- includes/admin-footer.php
+++ includes/admin-footer.php
@@ -13,7 +13,7 @@
 	
 	<footer>
 		<p>
-			This website was made with <img src="images/footer-pixels.png" alt="pixels" title="pixels"/> and lots of <img src="images/footer-heart.png" alt="love" title="love"/> by <a href="http://www.trishajessica.ca" target="_blank"><img src="images/footer-copyright.png" alt="© Trisha Jessica Designs" title="© Trisha Jessica Designs"/></a> 2012
+			This website was made with <img src="../images/footer-pixels.png" alt="pixels" title="pixels"/> and lots of <img src="../images/footer-heart.png" alt="love" title="love"/> by <a href="http://www.trishajessica.ca" target="_blank"><img src="../images/footer-copyright.png" alt="© Trisha Jessica Designs" title="© Trisha Jessica Designs"/></a> 2012
 		</p>
 	</footer>
 	<div class="footer-spacer"></div>
@@ -22,5 +22,5 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCkXYdCxOIr9-DwpF18ejWqV8C01jbmgxA&sensor=false"></script>
-<script src="js/splash-pad-locator.js"></script>
-<script src="js/latlng.min.js"></script>
+<script src="../js/splash-pad-locator.js"></script>
+<script src="../js/latlng.min.js"></script>
